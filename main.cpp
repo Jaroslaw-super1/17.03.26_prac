bd645edbf876685f2374e2c568fb6c9cde1f10e5
jar <Jarafan3@gmail.com>
2026-03-19 15:01:03 +0300

    Add many methods

--- main.cpp
+++ main.cpp
@@ -15,6 +15,27 @@ bool testVectorWithValue()
   return !v.isEmpety();
 }
 
+bool testCopyConstruct() // !!!!!!!!!!!!!!!!!!!!
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  topit::Vector< int > yav = v;
+  bool isAllEqual = v.getSize() == yav.getSize();
+  for (size_t i = 0; i < v.getSize(); ++i)
+  {
+
+  }
+  return isAllEqual;
+}
+
+bool testElementAssert()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  return v[0] == 1 && v[1] == 2;
+}
+
 int main()
 {
   using test_t = bool(*)();
@@ -23,6 +44,8 @@ int main()
   {
     {"Default vector is empety", testDefaultVector},
     {"Vector with any value is not empety", testVectorWithValue}
+    {"3 ", testCopyConstruct},
+    {"4 ", testElementAssert},
   };
   const size_t count = sizeof(tests) / sizeof(pair_t);
   std::cout << std::boolalpha;
@@ -36,6 +59,4 @@ int main()
   }
 
   std::cout << pass << '\n';
-
-//  std::cout << "testDefaultVector " << testDefaultVector() << '\n';
 }
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -2,6 +2,7 @@
 #define TOP_IT_VECTOR_HPP
 
 #include <iostream>
+#include <cassert>
 
 namespace topit
 {
@@ -13,24 +14,115 @@ namespace topit
     ~Vector();
     Vector< T > & operator=(const Vector< T > &) = delete;
 
+    void swap(Vector< T > & rhs) noexcept;
+
     bool isEmpety() const noexcept;
     size_t getSize() const noexcept;
     size_t getCapacity() const noexcept;
 
+    T & operator[](size_t id) noexcept;
+    const T & operator[](size_t id) const noexcept;
+
+    Vector< T > & operator=(const Vector< T > & rhs);
+
     void pushBack(const T &);
     void popBack();
+
+    void pushFront(const T &);
+    void popFront();
 
    private:
     T * data_;
     size_t size_, capacity_;
+
+    explicit Vector(size_t k);
   };
+}
+
+template< class T >
+void topit::Vector< T >::pushFront(const T &)
+{
+  Vector< T > cpy(val.getSize() + 1);
+
+  cpy[0] = val;
+  for (size_t i = 0; i < cpy.getSize(); i++)
+  {
+    cpy[i] = (*this)[i - 1];
+  }
+  swap(cpy);
+}
+
+template< class T >
+void topit::Vector< T >::swap(Vector< T > & rhs) noexcept
+{
+  std::swap(cpy.data_, data_);
+  std::swap(cpy.size_, size_);
+  std::swap(cpy.capacity_, capacity_);
+}
+
+
+template< class T >
+topit::Vector< T > & topit::Vector< T >::operator=(const Vector< T > & rhs)
+{
+  Vector< T > cpy(rhs);
+  swap(cpy);
+
+  return *this;
+}
+
+template< class T >
+topit::Vector< T >::Vector(size_t k):
+  data_(new T[k]),
+  size_(k),
+  capacity_(k)
+{
+  try {
+    for (size_t i = 0; i < getSize(); i++)
+    {
+      data_[i] = rhs[i];
+    }
+  } catch (...)
+  {
+
+  }
+}
+
+
+template< class T >
+topit::Vector< T >::Vector< T >(const Vector< T > & rhs):
+  Vector(rhs.getSize())
+{
+  for (size_t i = 0; i < getSize(); i++)
+  {
+    data_[i] = rhs[i];
+  }
+}
+
+template< class T >
+size_t topit::Vector< T >::getSize() const noexcept
+{
+  return size_;
 }
 
 template< class T >
 bool topit::Vector< T >::isEmpety() const noexcept
 {
+  assert(id < size());
   return !size_;
 }
+
+template< class T >
+T & topit::Vector< T >::operator[](size_t id) noexcept;
+{
+  return data_[0];
+}
+
+template< class T >
+const T & topit::Vector< T >::operator[](size_t id) const noexcept;
+{
+  return data_[0];
+}
+
 template< class T >
 void topit::Vector< T >::pushBack(const T &)
 {
